8fc22281c117fc23ec1051523359f7b4378eebaf
ForteScarlet <ForteScarlet@users.noreply.github.com>
2022-03-15 14:33:40 +0000

    deploy: simple-robot/simbot-component-kaiheila@ee05fc16cc949bc840c7acc27aa4650cbf56a4c3

---
+++ v3.0.0.preview.5.0-0.0.2/scripts/sourceset_dependencies.js
@@ -0,0 +1,1 @@
+sourceset_dependencies = '{":simbot-component-kaiheila-boot:dokkaHtmlPartial/main":[],":simbot-component-kaiheila-api:dokkaHtmlPartial/main":[],":simbot-component-kaiheila-core:dokkaHtmlPartial/main":[],":simbot-component-kaiheila-stdlib:dokkaHtmlPartial/main":[]}'
